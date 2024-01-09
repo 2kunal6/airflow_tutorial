c56d3023173e9d44d9d83b6b8e61e9fe4117ef88
Kunal Rout <2kunal6@gmail.com>
2024-01-09 19:17:19 +0530

    access via kwargs, jinja and context

--- src/dags/parameterized_dag.py
+++ src/dags/parameterized_dag.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
+
 # Accessing params directly
 def python_callable(params):
     print(params['param_name'])
@@ -8,6 +9,14 @@ def python_callable(params):
 # Accessing via context
 def python_callable_context(**context):
     print(context['params']['param_name'])
+
+# Accessing via jinja templating
+def python_callable_jinja(param_val):
+    print('{{ params.param_name }}')
+    print(param_val)
+
+def python_callable_kwargs(**kwargs):
+    print(kwargs['params']['param_name'])
 
 
 with DAG(dag_id='a_parameterized_dag',
@@ -26,4 +35,15 @@ with DAG(dag_id='a_parameterized_dag',
             "param_name": "task value"
         },
         python_callable=python_callable_context,
+    )
+
+    PythonOperator(
+        task_id="python_task_jinja",
+        op_args=['{{ params.param_name }}'],
+        python_callable=python_callable_jinja,
+    )
+
+    PythonOperator(
+        task_id="python_task_kwargs",
+        python_callable=python_callable_kwargs,
     )
