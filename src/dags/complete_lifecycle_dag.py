abba90ecb655a476e19d8cf3a08887dbc5d0b885
Kunal Rout <2kunal6@gmail.com>
2024-01-18 20:10:01 +0530

    using date passed as config

--- src/dags/complete_lifecycle_dag.py
+++ src/dags/complete_lifecycle_dag.py
@@ -17,11 +17,39 @@ def load_data(**kwargs):
     limit_query = ''
     if(Variable.get("environment") != 'prod'):
         limit_query = ' LIMIT 1000 '
-    sample_query = f'INSERT INTO {kwargs["db_schema_name"]}.SOURCE_{kwargs["app"]}_{kwargs["table_type"]} ' \
-                   f'SELECT * FROM {kwargs["db_schema_name"]}.DESTINATION_{kwargs["app"]}_{kwargs["table_type"]} ' \
+
+    # Check if data already loaded for this table and date, and if so then do nothing
+    sample_load_data_log = f'SELECT * FROM LOAD_DATA_METADATA_LOG' \
+                           f' WHERE table_name = {kwargs["db_schema_name"]}.DESTINATION_{kwargs["app"]}_{kwargs["table_type"]} ' \
+                           f' AND data_date = "{kwargs["run_date"]}" '
+
+
+    # Check if data is partially loaded.
+    # If data is partially loaded then there would be no entry in LOAD_DATA_METADATA_LOG table
+    sample_check_query = f'SELECT TOP 1 FROM {kwargs["db_schema_name"]}.DESTINATION_{kwargs["app"]}_{kwargs["table_type"]} ' \
+                   f'WHERE data_date = "{kwargs["run_date"]}" ' \
+                   f'LIMIT 1 '
+
+    # If partial data loaded then delete and load newly
+    sample_delete_query = f'DELETE FROM {kwargs["db_schema_name"]}.DESTINATION_{kwargs["app"]}_{kwargs["table_type"]} ' \
+                   f'WHERE data_date = "{kwargs["run_date"]}" '
+
+    # Load data
+    sample_load_data_query = f'INSERT INTO {kwargs["db_schema_name"]}.DESTINATION_{kwargs["app"]}_{kwargs["table_type"]} ' \
+                   f'SELECT * FROM {kwargs["db_schema_name"]}.SOURCE_{kwargs["app"]}_{kwargs["table_type"]} ' \
                    f'WHERE data_date = "{kwargs["run_date"]}" ' \
                    f'{limit_query}'
-    print(f'Running query: {sample_query}')
+
+    sample_insert_into_metadata_log_table = f'INSERT INTO LOAD_DATA_METADATA_LOG' \
+                    f' WHERE table_name = {kwargs["db_schema_name"]}.DESTINATION_{kwargs["app"]}_{kwargs["table_type"]} ' \
+                    f' AND data_date = "{kwargs["run_date"]}" '
+
+    sample_complete_query = '\n\n' + sample_load_data_log + \
+                            '\n\n' + sample_check_query + \
+                            '\n\n' + sample_delete_query + \
+                            '\n\n' + sample_load_data_query + \
+                            '\n\n' + sample_insert_into_metadata_log_table
+    print(f'Running query: {sample_complete_query}')
 
 def get_data_load_operator(app, table_type):
     return PythonOperator(task_id=f'{app}_{table_type}',
@@ -29,13 +57,15 @@ def get_data_load_operator(app, table_type):
                                      'table_type': table_type,
                                      'db_schema_name': config['environment_properties']['db_schema_name'],
                                      # If we use execution_date then we might get the wrong date if Airflow server goes down for an entire day
-                                     'run_date': '{{ data_interval_end }}'
+                                     'run_date': '{{ params.manual_data_load_date | default(data_interval_end) }}'
                                      },
                           python_callable=load_data)
 
 
 for dag_type in app_config['salesforce']:
-    with DAG(dag_id=dag_type) as dag:
+    with DAG(dag_id=dag_type,
+             params={"manual_data_load_date": "replace date with the date for which you want to load the data"}
+             ) as dag:
         send_metrics_task = PythonOperator(task_id = f'{dag_type}_metric_task',
                                            python_callable = send_metrics,
                                            trigger_rule = 'all_done',
